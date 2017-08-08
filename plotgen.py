59d77bbb7f01061ea62ae826a3df69a4fdc1be7a
Martin Weis <mwgit.20.mweis@spamgourmet.com>
2017-08-08 13:48:02 +0200

    second working version, creating two layers

--- plotgen.py
+++ plotgen.py
@@ -16,16 +16,16 @@
 #upper_left=vector
 
 # parcel size defs 
-##parcel_width=number 12
-##parcel_length=number 12
+##parcel_width=number 1.5
+##parcel_length=number 6
 
 # gaps
 ##gap_w=number 0
 ##gap_l=number 2
 
 # number of rows, cols
-##num_rows=number 100
-##num_cols=number 50
+##num_rows=number 10
+##num_cols=number 20
 
 
 # The vector output
@@ -50,7 +50,7 @@ attnames_line=["DATE","TIME","VERSION","ID","NAME","LENGTH","DIST1","DIST2","PRE
 crs = iface.mapCanvas().mapSettings().destinationCrs().toWkt()
 vector_grid = QgsVectorLayer('Polygon?crs='+ crs, result_name , 'memory')
 prov = vector_grid.dataProvider()
-vector_lines = QgsVectorLayer('Polygon?crs='+ crs, result_name + '=' , 'memory')
+vector_lines = QgsVectorLayer('LineString?crs='+ crs, result_name + '=' , 'memory')
 provl = vector_lines.dataProvider()
 
 # Add ids and coordinates fields
@@ -64,8 +64,6 @@ prov.addAttributes(fields)
 
 fieldsl = QgsFields()
 fieldsl.append(QgsField('trackid', QVariant.Int, '', 10, 0))
-fieldsl.append(QgsField('lineno', QVariant.Int, '', 10, 0))
-fieldsl.append(QgsField('ori', QVariant.Int, '', 10, 0))
 for an in attnames_line:
     fieldsl.append(QgsField(an, QVariant.String, '', 100, 0))
 provl.addAttributes(fields)
@@ -73,41 +71,37 @@ provl.addAttributes(fields)
 
 # Generate the features for the vector grid
 plotid = 0
-rowid=0
-colid=0
-y = ymax
-lineystart=ymax+vspacing
-lineyend=ymax - vspacing * num_rows - gap_l * num_rows - vspacing
-#while y >= ymin:
-while colid<num_cols:
-    colid+=1
-    x = xmin
-    rowid=0
-#    while x <= xmax:
-    while rowid <= num_rows:
-        rowid+=1
+# recompute ymin for the final geom
+ymin = ymax - (vspacing+gap_l)*num_rows
+lineystart = ymin - vspacing
+lineyend   = ymin + vspacing * num_rows + vspacing + gap_l * num_rows 
+linexstart = xmin - hspacing
+linexend   = xmin + hspacing * num_cols + hspacing + gap_w * num_cols
+for colid in range(num_cols):
+    x = xmin + (hspacing + gap_w)*colid
+    for rowid in range(num_rows):
+        y = ymin + (vspacing + gap_l)*rowid
+        plotid += 1
         point1 = QgsPoint(x, y)
         point2 = QgsPoint(x + hspacing, y)
-        point3 = QgsPoint(x + hspacing, y - vspacing)
-        point4 = QgsPoint(x, y - vspacing)
-        vertices = [point1, point2, point3, point4] # Vertices of the polygon for the current id
+        point3 = QgsPoint(x + hspacing, y + vspacing)
+        point4 = QgsPoint(x, y + vspacing)
         inAttr = [plotid, rowid, colid]
         feat = QgsFeature()
-        feat.setGeometry(QgsGeometry().fromPolygon([vertices])) # Set geometry for the current id
+        feat.setGeometry(QgsGeometry().fromPolygon([[point1, point2, point3, point4]])) # Set geometry for the current id
         feat.setAttributes(inAttr) # Set attributes for the current id
         prov.addFeatures([feat])
-        lpoint1 = QgsPoint(x+hspacing/2, lineystart)
-        lpoint2 = QgsPoint(x+hspacing/2, lineyend)
-        lvertices = [lpoint1, lpoint2]
-        lfeat = QgsFeature()
-        lfeat.setGeometry(QgsGeometry().fromPolyline([lvertices])) # Set geometry for the current id
-        lfeat.setAttributes(inAttr) # Set attributes for the current id
-        provl.addFeatures([lfeat])
-        x = x + hspacing + gap_w
-        plotid += 1
-    y = y - vspacing - gap_l
+    # generate the line features 
+    lpoint1 = QgsPoint(x + hspacing/2, lineystart)
+    lpoint2 = QgsPoint(x + hspacing/2, lineyend)
+    lfeat = QgsFeature()
+    lfeat.setGeometry(QgsGeometry().fromPolyline([lpoint1, lpoint2])) # Set line geometry for the current id
+    lfeat.setAttributes(inAttr) # Set attributes for the current id
+    inAttr = [colid]
+    provl.addFeatures([lfeat])
 
-# Update fields for the vector grid
+
+# Update fields for the vector
 vector_grid.updateFields()
 vector_lines.updateFields()
 
