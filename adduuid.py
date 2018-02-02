d5b777d4c96c30b53c4387d1274c1f6e1bea1010
Martin Weis <mwgit.20.mweis@spamgourmet.com>
2018-02-02 21:33:19 +0100

    adduuid comments

--- adduuid.py
+++ adduuid.py
@@ -32,5 +32,14 @@ for feature in iter:
     attrs = feature.attributes()
     # attrs is a list. It contains all the attribute values of this feature
     print attrs
+  
+#lyr=QgsVectorLayer('/media/martin/data700/martin/ltz-data/maxau-data/Hofgut_Maxau/gisdaten2018/grids/mx18_s16_soil.shp','addauuidhere','ogr') 
+#lf=lyr.getFeatures()
+#for c in f.fields().toList():
+#    print(c.name())
+# lp=lyr.dataProvider()
+# lp.addAttributes([QgsField("uuid",QVariant.String)])
+# lp.changeAttributeValues({1: uuid.uuid4()})
+# lyr.updateFields()
+# QgsFileWriter.writeAsVectorFormat(lyr,'out_uuid.shp',"utf-8",None,"ESRI Shapefile")
 
-    
